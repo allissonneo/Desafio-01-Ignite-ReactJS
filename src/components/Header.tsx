18530af8432165ce375a41dabd50573d216d909b
allissonneo <allissonneo@gmail.com>
2025-06-01 16:52:58 -0300

    Mudança na URL da logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src="https://raw.githubusercontent.com/allissonneo/Desafio-01-Ignite-ReactJS/458d9a39b816bedc322eaa6ed5cdf1a98c5ebe2f/public/logo.svg" alt="to.do"/>
       </div>
     </header>
   )
